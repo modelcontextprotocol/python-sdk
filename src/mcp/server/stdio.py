f46f4257d24f47673b244d2a5c0a44afbb8d5fd1
blackwhite084 <53146034+blackwhite084@users.noreply.github.com>
2024-12-24 20:54:54 +0800

    Update stdio.py

--- src/mcp/server/stdio.py
+++ src/mcp/server/stdio.py
@@ -29,6 +29,7 @@ import mcp.types as types
 
 import io
 sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
 @asynccontextmanager
