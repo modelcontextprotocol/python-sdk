7503489a38c12dac4679c8ade11e576ca00cf409
YUNYING <2281878254@qq.com>
2025-01-05 02:04:08 +0800

    fix: maybe linux has not reconfigure attr

--- src/mcp/server/stdio.py
+++ src/mcp/server/stdio.py
@@ -27,8 +27,9 @@ from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStre
 
 import mcp.types as types
 
-sys.stdin.reconfigure(encoding='utf-8')
-sys.stdout.reconfigure(encoding='utf-8')
+import io
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
 @asynccontextmanager
