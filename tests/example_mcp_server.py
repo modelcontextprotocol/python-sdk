d69d94e4ad60f3b7e5bfba18b2d3ea4b75496fdc
Thomas Sheffler <tom.sheffler@gmail.com>
2025-02-10 15:37:39 -0800

    Test and example MCP server to illustrate Issue #201. File mcp_stdio_client.py is adapted from the example in simple-chatbot.

---
+++ tests/example_mcp_server.py
@@ -0,0 +1,33 @@
+#
+# Small Demo server using FastMCP and illustrating debugging and notification streams
+#
+
+import logging
+from mcp.server.fastmcp import FastMCP, Context
+import time
+import asyncio
+
+mcp = FastMCP("MCP EXAMPLE SERVER", debug=True, log_level="DEBUG")
+
+logger = logging.getLogger(__name__)
+
+logger.debug(f"MCP STARTING EXAMPLE SERVER")
+
+@mcp.resource("config://app")
+def get_config() -> str:
+    """Static configuration data"""
+    return "Test Server 2024-02-25"
+
+@mcp.tool()
+async def simple_tool(x:float, y:float, ctx:Context) -> str:
+    logger.debug("IN SIMPLE_TOOL")
+    await ctx.report_progress(1, 2)
+    return x*y
+
+@mcp.tool()
+async def simple_tool_with_logging(x:float, y:float, ctx:Context) -> str:
+    await ctx.info(f"Processing Simple Tool")
+    logger.debug("IN SIMPLE_TOOL")
+    await ctx.report_progress(1, 2)
+    return x*y
+
---
+++ tests/test_mcp_tool.py
@@ -0,0 +1,83 @@
+import pytest
+
+import os
+import sys
+from .mcp_stdio_client import MCPClient
+
+from mcp import StdioServerParameters
+
+# locate the exmaple MCP server co-located in this directory
+
+mcp_server_dir = os.path.dirname(os.path.abspath(__file__))
+mcp_server_file = os.path.join(mcp_server_dir, "example_mcp_server.py")
+                           
+# mcpServers config in same syntax used by reference MCP
+
+servers_config = {
+    "mcpServers": {
+
+        "testMcpServer": {
+            "command": "mcp",   # be sure to . .venv/bin/activate so that mcp command is found
+            "args": [
+                "run",
+                mcp_server_file
+            ]
+        }
+
+    }
+}
+
+
+# @pytest.mark.asyncio
+@pytest.mark.anyio
+async def test_mcp():
+
+    servers = servers_config.get("mcpServers")
+
+    server0 = "testMcpServer"
+    config0 = servers[server0]
+    
+    client = MCPClient(
+        server0,
+        StdioServerParameters.model_validate(config0)
+    )
+    await client.initialize()
+    tools = await client.get_available_tools()
+
+    print(f"TOOLS:{tools}")
+    mcp_tool = tools[0]
+
+    res = await client.call_tool("simple_tool", {"x":5, "y":7})
+
+    print(f"RES:{res}")
+
+    # clients must be destroyed in reverse order
+    await client.cleanup()
+
+
+# @pytest.mark.asyncio
+@pytest.mark.anyio
+async def test_mcp_with_logging():
+
+    servers = servers_config.get("mcpServers")
+
+    server0 = "testMcpServer"
+    config0 = servers[server0]
+    
+    client = MCPClient(
+        server0,
+        StdioServerParameters.model_validate(config0)
+    )
+    await client.initialize()
+    tools = await client.get_available_tools()
+
+    print(f"TOOLS:{tools}")
+    mcp_tool = tools[0]
+
+    res = await client.call_tool("simple_tool_with_logging", {"x":5, "y":7})
+
+    print(f"RES:{res}")
+
+    # clients must be destroyed in reverse order
+    await client.cleanup()
+    
