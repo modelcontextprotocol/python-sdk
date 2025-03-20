9197f730e05550d24dd1fd7305044be420d1d5ab
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-03-20 08:07:01 +0000

    ignore

--- tests/client/test_list_roots_callback.py
+++ tests/client/test_list_roots_callback.py
@@ -3,7 +3,6 @@ from pydantic import FileUrl
 
 from mcp.client.session import ClientSession
 from mcp.server.fastmcp.server import Context
-from mcp.server.session import ServerSession
 from mcp.shared.context import RequestContext
 from mcp.shared.memory import (
     create_connected_server_and_client_session as create_session,
@@ -36,7 +35,7 @@ async def test_list_roots_callback():
         return callback_return
 
     @server.tool("test_list_roots")
-    async def test_list_roots(context: Context[ServerSession, None], message: str):
+    async def test_list_roots(context: Context, message: str):  # type: ignore[reportUnknownMemberType]
         roots = await context.session.list_roots()
         assert roots == callback_return
         return True
