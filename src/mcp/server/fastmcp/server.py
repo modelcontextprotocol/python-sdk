215cad3c9cedd2ee2e51b7784b6f3233c7e7b51b
tim-watcha <92134765+tim-watcha@users.noreply.github.com>
2025-04-19 13:05:55 +0900

    Remove comments

--- src/mcp/server/fastmcp/server.py
+++ src/mcp/server/fastmcp/server.py
@@ -72,7 +72,7 @@ class Settings(BaseSettings, Generic[LifespanResultT]):
     # HTTP settings
     host: str = "0.0.0.0"
     port: int = 8000
-    mount_path: str = "/"  # Mount path (e.g. "/github", defaults to root path)
+    mount_path: str = "/"
     sse_path: str = "/sse"
     message_path: str = "/messages/"
 
