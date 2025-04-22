d2f1146d496d1183ac0d386b53cc69510561fc7a
Lion Yang <lion@aosc.io>
2025-04-22 16:08:41 +0800

    Fix pyright warnings in test_stdio_cm.py

--- tests/client/test_stdio_cm.py
+++ tests/client/test_stdio_cm.py
@@ -3,14 +3,10 @@ import pytest
 from mcp import StdioServerParameters
 from mcp.client.stdio import stdio_client
 
-MCP_SERVER = {
-    "command": "tee",
-}
-
 
 @pytest.mark.anyio
 async def test_context_manager_exiting():
-    async with stdio_client(StdioServerParameters(**MCP_SERVER)) as (
+    async with stdio_client(StdioServerParameters(command="tee")) as (
         read_stream,
         write_stream,
     ):
