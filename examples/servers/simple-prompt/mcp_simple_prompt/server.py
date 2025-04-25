1917066e4daa03ddbb383a99e94d0c51a81d6626
PWZER <pwzergo@gmail.com>
2025-04-25 10:27:26 +0800

    update examples

--- examples/servers/simple-prompt/mcp_simple_prompt/server.py
+++ examples/servers/simple-prompt/mcp_simple_prompt/server.py
@@ -90,17 +90,23 @@ def main(port: int, transport: str) -> int:
     if transport == "sse":
         from mcp.server.sse import SseServerTransport
         from starlette.applications import Starlette
+        from starlette.responses import Response
         from starlette.routing import Mount, Route
 
         sse = SseServerTransport("/messages/")
 
         async def handle_sse(request):
-            async with sse.connect_sse(
-                request.scope, request.receive, request._send
-            ) as streams:
-                await app.run(
-                    streams[0], streams[1], app.create_initialization_options()
-                )
+            with anyio.CancelScope() as cancel_scope:
+                async with sse.connect_sse(
+                    request.scope,
+                    request.receive,
+                    request._send,
+                    lambda: cancel_scope.cancel(),
+                ) as streams:
+                    await app.run(
+                        streams[0], streams[1], app.create_initialization_options()
+                    )
+            return Response(status_code=200)
 
         starlette_app = Starlette(
             debug=True,
