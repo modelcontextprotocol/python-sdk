a84399b4a40ea5ee04f014a493cf94a6046ff466
633WHU <cliu_whu@yeah.net>
2025-05-26 11:00:44 +0800

    Streamable HTTP Trailing Slash Compatibility

--- examples/servers/simple-streamablehttp/mcp_simple_streamablehttp/server.py
+++ examples/servers/simple-streamablehttp/mcp_simple_streamablehttp/server.py
@@ -158,6 +158,7 @@ def main(
         debug=True,
         routes=[
             Mount("/mcp", app=handle_streamable_http),
+            Mount("/mcp/", app=handle_streamable_http),
         ],
         lifespan=lifespan,
     )
