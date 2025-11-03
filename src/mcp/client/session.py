5e6210918d66b863c183d5ff2bd226db8c53b7e9
Joaquin Coromina <bjoaquinc@gmail.com>
2025-11-03 01:58:46 +0100

    move functionality to after protocolVersion check

--- src/mcp/client/session.py
+++ src/mcp/client/session.py
@@ -167,11 +167,11 @@ class ClientSession(
             types.InitializeResult,
         )
 
-        if self._client_info is DEFAULT_CLIENT_INFO:
-            self._client_info = result.serverInfo
-
         if result.protocolVersion not in SUPPORTED_PROTOCOL_VERSIONS:
             raise RuntimeError(f"Unsupported protocol version from the server: {result.protocolVersion}")
+
+        if self._client_info is DEFAULT_CLIENT_INFO:
+            self._client_info = result.serverInfo
 
         await self.send_notification(types.ClientNotification(types.InitializedNotification()))
 
